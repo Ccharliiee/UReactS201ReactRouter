f58550cd24d52fb11e3218692816ccbbf44ba82e
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2023-03-06 17:57:19 -0500

    Add a 2nd route

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import HomeNavScrollExample from "./pages/home";
+import ProductContainerFluidExample from "./pages/products";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeNavScrollExample />,
+  },
+  {
+    path: "products",
+    element: <ProductContainerFluidExample />,
   },
 ]);
 
