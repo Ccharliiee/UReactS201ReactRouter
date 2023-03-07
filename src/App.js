82335052e7b88e47fe63eafbbdc2d1e155c0203a
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2023-03-07 18:26:45 -0500

    Update App.js
    rewrite react routes with createRoutesFromElements

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import HomeNavScrollExample from "./pages/home";
 import ProductContainerFluidExample from "./pages/products";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomeNavScrollExample />,
-  },
-  {
-    path: "products",
-    element: <ProductContainerFluidExample />,
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<HomeNavScrollExample />} />
+      <Route path="products" element={<ProductContainerFluidExample />} />
+    </Route>
+  )
+);
+
+// const routerobj = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <HomeNavScrollExample />,
+//   },
+//   {
+//     path: "products",
+//     element: <ProductContainerFluidExample />,
+//   },
+// ]);
 
 function App() {
   return <RouterProvider router={router} />;
