988ae613bd89d5d06e35876f493eb341149f4246
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2023-03-14 17:05:55 -0400

    add item list routes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import RootLayout from "./pages/rootLayout";
 import HomeNavScrollExample from "./pages/home";
 import ProductContainerFluidExample from "./pages/products";
+import ProductDetailPage from "./pages/productDetail";
 import { ErrorPage } from "./pages/errorpage";
 
 const router = createBrowserRouter(
@@ -15,6 +16,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
       <Route path="/" element={<HomeNavScrollExample />} />
       <Route path="products" element={<ProductContainerFluidExample />} />
+      <Route path="products/:productId" element={<ProductDetailPage />} />
     </Route>
   )
 );
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,5 +1,22 @@
-function ProductsPage() {
-  return <h1>The Products Page</h1>;
-}
+import { Link } from "react-router-dom";
+const ProductsPage = () => {
+  const DUMMYPRODUCTS = [
+    { id: "p1", title: "Product 1" },
+    { id: "p2", title: "Product 2" },
+    { id: "p3", title: "Product 3" },
+  ];
+  return (
+    <>
+      <h1>The Products Page</h1>
+      <ul>
+        {DUMMYPRODUCTS.map((prod) => (
+          <li key={prod.id}>
+            <Link to={prod.id}>{prod.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default ProductsPage;
