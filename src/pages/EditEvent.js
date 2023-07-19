ef19886c96943303a6d2a2a62c93d342be8b3966
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2023-07-18 21:40:00 -0400

    Update EditEvent.js
    retrieve target object from loaded data

--- src/pages/EditEvent.js
+++ src/pages/EditEvent.js
@@ -3,9 +3,8 @@ import { useRouteLoaderData } from "react-router-dom";
 import EventForm from "../component/EventForm";
 
 const EditEventPage = () => {
-  return (
-    <EventForm method="patch" event={useRouteLoaderData("event-detail")} />
-  );
+  const { eventData } = useRouteLoaderData("event-detail");
+  return <EventForm method="patch" event={eventData} />;
 };
 
 export default EditEventPage;
