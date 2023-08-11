274efac3d10c84f06880c7cef1b23ef5db96f50b
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2023-08-11 14:46:33 -0400

    Add lazy loading

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+import { lazy, Suspense } from "react";
+
 import RootLayout from "./pages/rootLayout";
 import HomePage from "./pages/home";
 import ProductRootLayout from "./pages/productsRoot";
 import ProductContainerFluidExample from "./pages/products";
 import ProductDetailPage from "./pages/productDetail";
-import EventsRootLayout from "./pages/EventsRoot";
-import EventsPage, { EventLoader } from "./pages/Events";
-import EventDetailPage, {
-  EventPageDataLoader,
-  deleteEventAction,
-} from "./pages/EventDetail";
-import EditEventPage from "./pages/EditEvent";
-import NewEventPage from "./pages/NewEvent";
+// import EventsRootLayout from "./pages/EventsRoot";
+// import EventsPage, { EventLoader } from "./pages/Events";
+// import EventDetailPage, {
+//   EventPageDataLoader,
+//   deleteEventAction,
+// } from "./pages/EventDetail";
+// import EditEventPage from "./pages/EditEvent";
+// import NewEventPage from "./pages/NewEvent";
 import { eventUploadAction } from "./component/EventForm";
 import NewsletterPage, { newsletterSignupAction } from "./pages/Newsletter";
 import { ErrorPage } from "./pages/errorpage";
@@ -27,6 +29,12 @@ import {
   getEventsAccessAuthToken,
   checkEventsAccessToken,
 } from "./utils/authUtil";
+
+const EventsRootLayout = lazy(() => import("./pages/EventsRoot"));
+const EventsPage = lazy(() => import("./pages/Events"));
+const EventDetailPage = lazy(() => import("./pages/EventDetail"));
+const EditEventPage = lazy(() => import("./pages/EditEvent"));
+const NewEventPage = lazy(() => import("./pages/NewEvent"));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -49,25 +57,66 @@ const router = createBrowserRouter(
         <Route path=":productId" element={<ProductDetailPage />} />
       </Route>
 
-      <Route path="/events" element={<EventsRootLayout />}>
-        <Route index element={<EventsPage />} loader={EventLoader} />
-        <Route path=":eventId" loader={EventPageDataLoader} id="event-detail">
+      <Route
+        path="/events"
+        element={
+          <Suspense fallback={<p>Loading...</p>}>
+            <EventsRootLayout />
+          </Suspense>
+        }
+      >
+        <Route
+          index
+          element={
+            <Suspense fallback={<p>Loading...</p>}>
+              <EventsPage />
+            </Suspense>
+          }
+          loader={() =>
+            import("./pages/Events").then((module) => module.EventLoader())
+          }
+        />
+        <Route
+          path=":eventId"
+          loader={({ request, params }) =>
+            import("./pages/EventDetail").then((module) =>
+              module.EventPageDataLoader({ request, params })
+            )
+          }
+          id="event-detail"
+        >
           <Route
             index
-            element={<EventDetailPage />}
-            action={deleteEventAction}
+            element={
+              <Suspense fallback={<p>Loading...</p>}>
+                <EventDetailPage />
+              </Suspense>
+            }
+            action={({ request, params }) =>
+              import("./pages/EventDetail").then((module) =>
+                module.deleteEventAction({ request, params })
+              )
+            }
           />
           <Route
             path="edit"
             loader={checkEventsAccessToken}
-            element={<EditEventPage />}
+            element={
+              <Suspense fallback={<p>Loading...</p>}>
+                <EditEventPage />
+              </Suspense>
+            }
             action={eventUploadAction}
           />
         </Route>
         <Route
           path="new"
           loader={checkEventsAccessToken}
-          element={<NewEventPage />}
+          element={
+            <Suspense fallback={<p>Loading...</p>}>
+              <NewEventPage />
+            </Suspense>
+          }
           action={eventUploadAction}
         />
       </Route>
